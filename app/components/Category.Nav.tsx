b6d8b626ddbd3e6239478a857c69da1c4a67d316
Urnuun <YOUR-EMAIL-HERE>
2026-07-23 13:35:22 +0800

    Fix Category.Nav component name for Vercel deployment

--- app/components/Category.Nav.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Category.Nav = () => {
-  return (
-    <div>Category.Nav</div>
-  )
-}
---
+++ app/components/CategoryNav.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const CategoryNav = () => {
+  return (
+    <div>CategoryNav</div>
+  )
+}
